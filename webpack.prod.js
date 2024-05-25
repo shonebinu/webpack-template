09987fb2273fe54ff56c60f85fe8531bdc04a681
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-05-25 13:40:08 +0530

    add babel transpiler to production

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,4 +4,18 @@ const common = require("./webpack.common.js");
 module.exports = merge(common, {
 	mode: "production",
 	devtool: "source-map", // to debug in production
+	module: {
+		rules: [
+			{
+				test: /\.(?:js|mjs|cjs)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: [["@babel/preset-env", { targets: "defaults" }]],
+					},
+				},
+			},
+		],
+	},
 });
