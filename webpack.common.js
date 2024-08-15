c055f90fad6d33ea5a21807f86b328d7328cc97c
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-08-15 18:20:23 +0530

    add html loader

--- webpack.common.js
+++ webpack.common.js
@@ -22,6 +22,10 @@ module.exports = {
 				use: ["style-loader", "css-loader"],
 			},
 			{
+				test: /\.html$/i,
+				loader: "html-loader",
+			},
+			{
 				test: /\.(png|svg|jpg|jpeg|gif)$/i,
 				type: "asset/resource",
 			},
