ae33320451b010b77d87332cf507e16439b8b675
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-08-22 06:25:08 +0530

    configure jest plugin for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,22 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import jestPlugin from "eslint-plugin-jest";
 
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
+	{ languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+	pluginJs.configs.recommended,
+	{
+		files: ["**/*.test.js", "**/*.spec.js"],
+		plugins: {
+			jest: jestPlugin,
+		},
+		languageOptions: {
+			globals: {
+				...globals.jest,
+			},
+		},
+		rules: {
+			...jestPlugin.configs.recommended.rules,
+		},
+	},
 ];
