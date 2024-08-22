bbceb578aa2cc4bd6079416cee1d6b0c4670c349
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-08-22 06:28:09 +0530

    refactor jest plugin for eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,18 +5,5 @@ import jestPlugin from "eslint-plugin-jest";
 export default [
 	{ languageOptions: { globals: { ...globals.browser, ...globals.node } } },
 	pluginJs.configs.recommended,
-	{
-		files: ["**/*.test.js", "**/*.spec.js"],
-		plugins: {
-			jest: jestPlugin,
-		},
-		languageOptions: {
-			globals: {
-				...globals.jest,
-			},
-		},
-		rules: {
-			...jestPlugin.configs.recommended.rules,
-		},
-	},
+	jestPlugin.configs.recommended,
 ];
